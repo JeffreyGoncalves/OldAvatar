d1bcd1a5420f25f55056f5ed15a4437e44be84c1
Oummar Mayaki <oummar.mayaki@gmail.com>
2018-06-21 16:38:48 +0100

    Sound bar delay fixed

--- LecturerTrainer/Model/ReplayAvatar.cs
+++ LecturerTrainer/Model/ReplayAvatar.cs
@@ -605,14 +605,14 @@ namespace LecturerTrainer.Model
 			GL.PushAttrib(AttribMask.ColorBufferBit);
 			wiggleIndex = currentWiggleIndex();
 
-            for (int i = 0;  i < 299; i++)
+            for (int i = 130;  i < 299; i++)
             {
 				
 				yw = +0.6f + replaySoundBar[i + wiggleIndex].Item2 / 500.0f; 
 				yw1 = +0.6f + replaySoundBar[i + 1 + wiggleIndex].Item2 / 500.0f;
 
-                xw = -2.5f + i / 60.0f;
-                xw1 = -2.5f + (i + 1) / 60.0f;
+                xw = -3.6f + i / 60.0f;
+                xw1 = -3.6f + (i + 1) / 60.0f;
 
                 GL.PushMatrix();
                 GL.Begin(PrimitiveType.Lines);
